e99d696d1b02f675587688aa43174b0013c008be
goingdust <dustinluck@gmail.com>
2021-12-01 22:38:31 +0000

    Made IP and PORT constant values in ./constants and imported/exported them to client.js

--- input.js
+++ input.js
@@ -19,9 +19,34 @@ const setupInput = function (conn) {
       conn.write("Move: down");
     } else if (data === 'd') {
       conn.write("Move: right");
-    } else if (data === 'b') {
+    } else if (data === 'u') {
       conn.write("Say: uwuwuwuwu");
     }
+
+    // const quit = () => {
+    //   console.log("thanks for playing, bye bye! \n");
+    //   process.exit();
+    // };
+    // const MOVE_UP_KEY = conn.write("Move: up"); // need to make these functions for them to work
+    // const MOVE_LEFT_KEY = conn.write("Move: up");
+    // const MOVE_DOWN_KEY = conn.write("Move: up");
+    // const MOVE_RIGHT_KEY = conn.write("Move: up");
+    // const SPEAK_KEY = conn.write("Say: uwuwuwuwu");
+    
+    // const keyCommands = {
+    //   '\u0003': quit,
+    //   'w': MOVE_UP_KEY,
+    //   'a': MOVE_LEFT_KEY,
+    //   's': MOVE_DOWN_KEY,
+    //   'd': MOVE_RIGHT_KEY,
+    //   'u': SPEAK_KEY
+    // };
+
+    // for (key in keyCommands) {
+    //   if (data === key) {
+    //     keyCommands[key];
+    //   }
+    // }
   };
 
   stdin.on("data", handleUserInput);
