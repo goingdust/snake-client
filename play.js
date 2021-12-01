a2f2e04e5a801e003e0784f2bd6f08379c6a71d3
goingdust <dustinluck@gmail.com>
2021-12-01 20:11:10 +0000

    Inserted setupInput and wrote handleUserInput so that we can disconnect from the game via pressing Ctrl + C

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    console.log("thanks for playing, bye bye! \n");
+    process.exit();
+  }
+};
+
 console.log("Connecting ...");
+setupInput();
 connect();
